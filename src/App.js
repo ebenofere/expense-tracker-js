eb29da90abaa7050fdeaaea272b7a2dde48aeb02
ebenofere <eofere@gmail.com>
2022-04-27 15:30:57 +0100

    adding normal JavaScript

--- src/App.js
+++ src/App.js
@@ -35,8 +35,16 @@ function App() {
   return (
     <div>
       <h2>Let's get started</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem({ title, amount }) {
+function ExpenseItem({ title, amount, date }) {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item_description">
         <h2>{title}</h2>
         <div className="expense-item_price">${amount}</div>
