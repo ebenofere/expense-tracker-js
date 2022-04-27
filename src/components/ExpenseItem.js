e171fad7d14f399edd13837a88d41a8390ba21c9
ebenofere <eofere@gmail.com>
2022-04-27 15:03:19 +0100

    passing data as props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem({ title, amount }) {
   return (
     <div className="expense-item">
       <div>March 28th 2021</div>
       <div className="expense-item_description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item_price">$294.67</div>
+        <h2>{title}</h2>
+        <div className="expense-item_price">${amount}</div>
       </div>
     </div>
   );
