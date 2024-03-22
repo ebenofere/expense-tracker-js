949578dfcf19f76655c39aeeb6c906ae4b4c3c8f
ebenofere <eofere@gmail.com>
2024-03-21 23:26:50 -0400

    update

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ const form = document.getElementById("form");
 const text = document.getElementById("text");
 const amount = document.getElementById("amount");
 
-const localStorageTransactions = JSON.parse(localStorage.getItem("transaction"));
+const localStorageTransactions = JSON.parse(localStorage.getItem("transactions"));
 
-let transactions = localStorage.getItem("transaction") != null ? localStorageTransactions : [];
+let transactions = localStorage.getItem("transactions") != null ? localStorageTransactions : [];
 
 // function to add transaction
 function addTransaction(e) {
@@ -17,10 +17,10 @@ function addTransaction(e) {
   if (text.value.trim() === "" || amount.value.trim() === "") {
     alert("please enter a text and amount");
   } else {
-    let transaction = {
+    const transaction = {
       id: generateID(),
       text: text.value,
-      amount: amount.value
+      amount: +amount.value
     }
 
     transactions.push(transaction);
@@ -49,9 +49,9 @@ function addTransactionDOM(transaction) {
   const sign = transaction.amount < 0 ? "-" : "+";
   const item = document.createElement("li");
 
-  item.classList.add(`${transaction < 0 ? "minus" : "plus"}`);
+  item.classList.add(transaction.amount < 0 ? "minus" : "plus");
 
-  item.innerHTML = `${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn" onclick=${removeTransaction(transaction.id)}>x</button></li>`;
+  item.innerHTML = `${transaction.text} <span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>`;
 
   list.appendChild(item); 
 }
